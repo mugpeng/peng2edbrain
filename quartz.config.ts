e10f2a364b5e3caad4041b07ff1e566cf5d5e959
Peng <52995448+mugpeng@users.noreply.github.com>
2025-07-07 12:50:46 +0800

    Quartz sync: Jul 7, 2025, 12:50 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Peng's 2ed brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
